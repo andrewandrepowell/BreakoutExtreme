82c19f5bd40c522ec4927723105e44ead1781897
Andrew Andre Powell <andrewandrepowell2@gmail.com>
2025-02-19 00:43:23 -0600

    Updating GitHub pages for temporary hosting.

---
+++ docs/js/utility.js
@@ -0,0 +1,7 @@
+﻿export function RegisterServiceUpdates(dotnetHelper)
+{
+    window.document.addEventListener('touchstart', (event) => {
+        // Adding this empty event listener allows for dragging
+        // finger accross touch screen on safari.
+    })
+}
