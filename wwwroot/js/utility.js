346ad684605854957a7c3d3c551d1b14348d52b4
Andrew Andre Powell <andrewandrepowell2@gmail.com>
2025-02-26 22:36:28 -0600

    Added fix to prevent the magnifying glass from appearing on chrome and safari.

--- wwwroot/js/utility.js
+++ wwwroot/js/utility.js
@@ -4,4 +4,10 @@
         // Adding this empty event listener allows for dragging
         // finger accross touch screen on safari.
     })
+    window.document.addEventListener('touchend', (event) => {
+        // Needed this to prevent the magnifying glass from appearing on safari and chrome.
+    })
+    window.document.addEventListener('click', (event) => {
+        // Needed this to prevent the magnifying glass from appearing on safari and chrome.
+    })
 }
