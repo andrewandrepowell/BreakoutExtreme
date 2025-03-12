624ea49e6ace756cd4c515a284e1a8c77ee180a8
Andrew Andre Powell <andrewandrepowell2@gmail.com>
2025-03-12 01:07:48 -0500

    Updating GitHub Pages with Game.

---
+++ docs/js/utility.js
@@ -0,0 +1,13 @@
+﻿export function RegisterServiceUpdates(dotnetHelper)
+{
+    window.document.addEventListener('touchstart', (event) => {
+        // Adding this empty event listener allows for dragging
+        // finger accross touch screen on safari.
+    })
+    window.document.addEventListener('touchend', (event) => {
+        // Needed this to prevent the magnifying glass from appearing on safari and chrome.
+    })
+    window.document.addEventListener('click', (event) => {
+        // Needed this to prevent the magnifying glass from appearing on safari and chrome.
+    })
+}
